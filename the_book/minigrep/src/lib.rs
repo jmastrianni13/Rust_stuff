bf87cd8a9da75761ca78627d64cefba8a19e0a85
Joe Mastrianni <jmastrianni13@gmail.com>
2023-10-18 19:37:30 -0400

    moved logic to lib.rs

---
+++ the_book/minigrep/src/lib.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    return Ok(());
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, String>{
+        if args.len() < 3 {
+            let err_msg = format!("expect 2 arguments, got {}", args.len()-1);
+            return Err(err_msg);
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        return Ok(Config{query, file_path});
+    }
+}
+
