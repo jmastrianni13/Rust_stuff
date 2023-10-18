403215152fbed7af3a5ebceb22712f9eb8bfe3df
Joe Mastrianni <jmastrianni13@gmail.com>
2023-10-18 18:34:54 -0400

    further improve error handling

--- the_book/minigrep/src/main.rs
+++ the_book/minigrep/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
    
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -20,14 +24,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, String>{
         if args.len() < 3 {
-            panic!("expect 2 arguments, got {}", args.len() - 1);
+            let err_msg = format!("expect 2 arguments, got {}", args.len()-1);
+            return Err(err_msg);
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        return Config{query, file_path};
+        return Ok(Config{query, file_path});
     }
 }
 
