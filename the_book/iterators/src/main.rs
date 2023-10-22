54d69c0489e2c19fd29632ae997d9f333b0eee01
Joe Mastrianni <jmastrianni13@gmail.com>
2023-10-21 21:37:35 -0400

    initial commit

---
+++ the_book/iterators/src/main.rs
@@ -0,0 +1,55 @@
+fn main() {
+    vec_iter();
+}
+
+
+fn vec_iter() {
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+
+    for v in v1_iter {
+        println!("> {v}");
+    }
+}
+
+#[test]
+fn iterator_demonstration() {
+    let v1 = vec![1, 2, 3];
+
+    let mut v1_iter = v1.iter();
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
+
+#[test]
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    let total: i32 = v1_iter.sum();
+
+    assert_eq!(total, 6);
+}
+
+#[test]
+fn iterator_adopter() {
+    // doesn't consume the iterator
+    let v1: Vec<i32> = vec![1, 2, 3];
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2, vec![2, 3, 4]);
+
+}
+
+//pub trait Iterator {
+  //  type Item:
+
+ //   fn next(&mut self) -> Option<Self::Item>;
+
+    // methods with default implementations elided
+
+//}
