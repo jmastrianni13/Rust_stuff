9f6840301d4ee20dbac351206bc0c0c15ead4fe3
Joe Mastrianni <jmastrianni13@gmail.com>
2023-10-24 22:08:13 -0400

    simple loop that echos input text

--- shells/simple_shell/src/main.rs
+++ shells/simple_shell/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the simple shell");
+
+    loop {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        let line = line.trim();
+        println!("{line}");
+    }
 }
