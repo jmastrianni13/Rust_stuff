08f60eefc3f9c4bd417104ed252222f05bc7f9c2
Joe Mastrianni <jmastrianni13@gmail.com>
2023-10-24 22:42:41 -0400

    get and print path

--- shells/simple_shell/src/main.rs
+++ shells/simple_shell/src/main.rs
@@ -2,6 +2,8 @@ use std::io;
 fn main() {
     println!("Welcome to the simple shell");
 
+    let path = std::env::var("PATH").unwrap_or_else(|_| String::new());
+    println!("{path}");
     loop {
         let mut line = String::new();
         io::stdin().read_line(&mut line).unwrap();
