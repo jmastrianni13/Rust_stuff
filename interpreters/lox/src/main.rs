87747ed50ff4d33326cde264f48cd2d2232822a9
Joe Mastrianni <jmastrianni13@gmail.com>
2023-10-26 01:01:08 -0400

    added run_file, run_prompt

--- interpreters/lox/src/main.rs
+++ interpreters/lox/src/main.rs
@@ -1,7 +1,26 @@
 use std::env;
+use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    dbg!(args);
+    if args.len() > 2 {
+        println!("Usage: lox [script]");
+        process::exit(64);
+    } else if args.len() == 2 {
+        run_file(args[1].as_str());
+    } else {
+        run_prompt();
+    }
 }
+
+fn run_file(path: &str) {
+    let contents = fs::read_to_string(path)
+        .expect("could not read file");
+}
+
+fn run_prompt() {
+
+}
+
