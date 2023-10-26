5e63ff44942fc1738472cda3644bb4d73f289fc9
Joe Mastrianni <jmastrianni13@gmail.com>
2023-10-26 00:38:36 -0400

    wip

--- shells/simple_shell/src/main.rs
+++ shells/simple_shell/src/main.rs
@@ -1,7 +1,8 @@
-use std::io;
+use std::(io, path::PathBuf);
+
 fn main() {
     println!("Welcome to the simple shell");
-
+    let vars: Vec<String> = std::env::args().collect();
     let path = std::env::var("PATH").unwrap_or_else(|_| String::new());
     println!("{path}");
     loop {
@@ -10,4 +11,30 @@ fn main() {
         let line = line.trim();
         println!("{line}");
     }
+}
+
+fn run_process(vars: &Vec<String>, command: &str) -> Result<(), ()> {
+    return Ok(());
+}
+
+fn find_binary(command: &str) -> Result<PathBuf, std::io::Error> {
+    fn search(command: &str, path: &Path) -> Result<(), std::io::Error> {
+        for entry in std::fs::read_dir(path)? {
+            if let Ok(entry) = entry {
+                if let Ok(met) = entry.metadata() {
+                    if met.is_file() || met.is_symlink() {
+                        if let Some(name) = entry.path().file_name() {
+                            if name == command {
+                                if met.is_symlink() {
+                                    panic!("Running symlinks not supported");
+                                }
+                                return Ok(());
+                            }
+                        }
+                    }
+                 }
+            }
+        }
+        Err(std::io::ErrorKind::NotFound.into())
+    };
 }
