3376c2985c2c0aebb8d5dd9e6d3d19970feb2b83
Joe Mastrianni <jmastrianni13@gmail.com>
2023-10-26 22:06:56 -0400

    updated token, added token type

--- interpreters/lox/src/scanner.rs
+++ interpreters/lox/src/scanner.rs
@@ -11,4 +11,78 @@ impl Scanner {
 }
 
 #[derive(Debug)]
-pub struct Token {}
+pub struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: Option<LiteralValue>,
+    line_number: u64,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, literal: Option<LiteralValue>, line_number: u64) -> Self {
+        Self {
+            token_type,
+            lexeme,
+            literal,
+            line_number,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum LiteralValue {
+    IntValue(i64),
+    FValue(f64),
+    StringValue(String),
+    IdentifierVal(String)
+}
+
+#[derive(Debug)]
+pub enum TokenType {
+    // single-character tokens
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Start,
+
+    // one or two character tokens
+    Bang,
+    BangEqual,
+    Equal,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // literals
+    Identifier,
+    String,
+    Number,
+
+    // keywords
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof
+}
