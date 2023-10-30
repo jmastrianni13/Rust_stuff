1198581ee3ad31f04947499b67648a54e2121b65
Joe Mastrianni <jmastrianni13@gmail.com>
2023-10-29 21:57:46 -0400

    more token work, lexemes

--- interpreters/lox/src/scanner.rs
+++ interpreters/lox/src/scanner.rs
@@ -18,9 +18,13 @@ impl Scanner {
     }
 
     pub fn scan_tokens(self: &mut Self) -> Result<Vec<Token>, String> {
+        let mut errors = vec![];
         while !self.is_at_end() {
             self.start = self.current;
-            self.scan_token()?;
+            match self.scan_token() {
+                Ok(_) => (),
+                Err(msg) => errors.push(msg),
+            }
         }
 
         self.tokens.push(Token {
@@ -30,11 +34,62 @@ impl Scanner {
             line_number: self.line,
         });
 
+        if errors.len() > 0 {
+            let mut joined = "".to_string();
+            errors.iter().map(|msg| {
+                joined.push_str(&msg);
+                joined.push_str("\n");
+            });
+            return Err(joined);
+        }
+
         return Ok(self.tokens.clone());
     }
 
-    fn scan_token(self: &mut Self) -> Result<Token, String> {
+    fn scan_token(self: &mut Self) -> Result<(), String> {
+        let c = self.advance();
+
+        match c {
+            '(' => self.add_token(TokenType::LeftParen),
+            ')' => self.add_token(TokenType::RightParen),
+            '{' => self.add_token(TokenType::LeftBrace),
+            '}' => self.add_token(TokenType::RightBrace),
+            ',' => self.add_token(TokenType::Comma),
+            '.' => self.add_token(TokenType::Dot),
+            '-' => self.add_token(TokenType::Minus),
+            '+' => self.add_token(TokenType::Plus),
+            ';' => self.add_token(TokenType::Semicolon),
+            '*' => self.add_token(TokenType::Star),
+            _ => return Err(format!("unrecognized char at line {}: {}", self.line, c))
+        }
+
         todo!()
+    }
+
+    fn advance(self: &mut Self) -> char {
+        let c = self.source.as_bytes()[self.current];
+        self.current += 1;
+
+        return c as char;
+    }
+
+    fn add_token(self: &mut Self, token_type: TokenType) {
+        self.add_token_lit(token_type, None);
+    }
+
+    fn add_token_lit(self: &mut Self, token_type: TokenType, literal: Option<LiteralValue>) {
+        let mut text = "".to_string();
+        let bytes = self.source.as_bytes();
+        for i in self.start..self.current {
+            text.push(bytes[i] as char);
+        }
+
+        self.tokens.push(Token {
+            token_type: token_type,
+            lexeme: text,
+            literal: literal,
+            line_number: self.line,
+        });
     }
 
     fn is_at_end(self: &Self) -> bool {
@@ -87,7 +142,7 @@ pub enum TokenType {
     Plus,
     Semicolon,
     Slash,
-    Start,
+    Star,
 
     // one or two character tokens
     Bang,
