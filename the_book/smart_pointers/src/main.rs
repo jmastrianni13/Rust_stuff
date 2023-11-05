f25e051b9d4bd8a2e0d1a7c5d693f3558bbf6aae
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-05 01:38:37 -0400

    added memory leak example

--- the_book/smart_pointers/src/main.rs
+++ the_book/smart_pointers/src/main.rs
@@ -33,6 +33,7 @@ fn main() {
 
     demo_refcell_rc();
 
+    demo_memleak();
 }
 
 fn get_box(x: i32) -> Box<i32> {
@@ -105,6 +106,21 @@ enum RefCellRcLispList {
     Nil,
 }
 
+#[derive(Debug)]
+enum MemLeakList {
+    Cons(i32, RefCell<Rc<MemLeakList>>),
+    Nil,
+}
+
+impl MemLeakList {
+    fn tail(&self) -> Option<&RefCell<Rc<MemLeakList>>> {
+        match self {
+            MemLeakList::Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
 fn demo_rc() {
     // commented out lines do not compile because a is moved when b is created
     // let a = LispList::Cons(5, Box::new(LispList::Cons(10, Box::new(LispList::Nil))));
@@ -154,5 +170,31 @@ fn demo_refcell_rc() {
     println!("b = {:?}", b);
     println!("c = {:?}", c);
 
+}
+
+fn demo_memleak() {
+    let a = Rc::new(MemLeakList::Cons(5, RefCell::new(Rc::new(MemLeakList::Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(MemLeakList::Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b); // replace last elem of a with a reference to b
+    }
+
+    // ref count of 2 demonstrates memory leak
+    // once a (b) is dropped the ref count is reduced to 1 (1) instead of 0 and neither object will
+    // be cleaned up
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the last line to see the cycle created in this function (stack overflow occurs)
+    // println!("a next item = {:?}", a.tail());
 }
 
