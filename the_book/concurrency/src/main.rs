36878ca93b6a0330456845bd40adb457ac87641b
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-06 20:56:02 -0500

    added thread demo

--- the_book/concurrency/src/main.rs
+++ the_book/concurrency/src/main.rs
@@ -1,3 +1,21 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    demo_threads();
+}
+
+fn demo_threads() {
+
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1000));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {i} from the main thread!");
+        thread::sleep(Duration::from_millis(500));
+    }
 }
