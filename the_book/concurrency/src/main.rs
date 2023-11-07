5b57180e184d30fb4add8c2e8d6d1149f92ddf1e
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-06 21:08:55 -0500

    added join examples

--- the_book/concurrency/src/main.rs
+++ the_book/concurrency/src/main.rs
@@ -7,6 +7,15 @@ fn main() {
 
 fn demo_threads() {
 
+    let blockinghandle = thread::spawn(|| {
+        for i in 1..4 {
+            println!("hi number {i} from the blocking joinhandle thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    blockinghandle.join().unwrap(); // will block any threads started below this line
+
     thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {i} from the spawned thread!");
@@ -14,8 +23,18 @@ fn demo_threads() {
         }
     });
 
+    let handle = thread::spawn(|| {
+        for i in 1..8 {
+            println!("hi number {i} from the joinhandle thread!");
+            thread::sleep(Duration::from_millis(750));
+        }
+    });
+
     for i in 1..5 {
         println!("hi number {i} from the main thread!");
         thread::sleep(Duration::from_millis(500));
     }
+
+    handle.join().unwrap() // main thread will wait for this thread to finish
 }
+
