6888dd0cae14d43bf22923090211dfccc5d7b570
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-06 20:33:17 -0500

    wip equality parsing

--- interpreters/lox/src/parser.rs
+++ interpreters/lox/src/parser.rs
@@ -1,7 +1,41 @@
+use crate::expr;
 use crate::scanner;
 
-public struct Parser {
+pub struct Parser {
     tokens: Vec<scanner::Token>,
     current: usize,
+}
+
+impl Parser {
+    pub fn new(tokens: Vec<scanner::Token>) -> Self {
+        Self {
+            tokens: tokens,
+            current: 0,
+        }
+    }
+
+    fn expression(&mut self) -> expr::Expr {
+        self.equality()
+    }
+
+    fn equality(&mut self) -> expr::Expr {
+        let mut exp = self.comparison();
+
+        while self.match_token(scanner::TokenType::BangEqual, scanner::TokenType::EqualEqual) {
+            let operator = self.previous();
+            let rhs = self.comparison();
+            let exp = expr::Expr::Binary {
+                left: exp,
+                operator: operator,
+                right: rhs
+            };
+
+            return exp;
+
+        }
+
+        todo!();
+    }
+
 }
 
