b536ab5156ccc9e98e22b337f74f883cb6526fa1
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-06 22:11:34 -0500

    added more messages to mspc demo

--- the_book/concurrency/src/main.rs
+++ the_book/concurrency/src/main.rs
@@ -56,11 +56,22 @@ fn demo_mpsc() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    let msg = rx.recv().unwrap();
-    println!("Got: {msg}");
+    for msg in rx {
+        println!("Got: {}", msg);
+    };
+
 }
 
