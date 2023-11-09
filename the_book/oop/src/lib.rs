e106811584ed64bfe22b8604669075d4654f59ca
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-08 21:37:34 -0500

    encapsulation example added methods

--- the_book/oop/src/lib.rs
+++ the_book/oop/src/lib.rs
@@ -1,5 +1,39 @@
-pub struct AverageCollection {
+pub struct AveragedCollection {
     list: Vec<i32>,
-    average: f64,
+    average: Option<f64>,
+}
+
+impl AveragedCollection {
+    pub fn new() -> Self {
+        return Self {
+            list: vec![],
+            average: None
+        };
+    }
+
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+        match result {
+            Some(value) => {
+                self.update_average();
+                return Some(value);
+            }
+            None => None,
+        }
+    }
+
+    pub fn get_average(&self) -> f64 {
+        return self.average.unwrap();
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = Some(total as f64 / self.list.len() as f64);
+    }
 }
 
