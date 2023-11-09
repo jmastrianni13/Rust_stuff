d696ab90ace4ebb0ebfca1df3e812297c93190dc
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-08 21:37:55 -0500

    encapsulation example added demo

--- the_book/oop/src/main.rs
+++ the_book/oop/src/main.rs
@@ -1,3 +1,19 @@
+use oop;
 fn main() {
-    println!("Hello, world!");
+    demo_averagedcollection();
 }
+
+fn demo_averagedcollection () {
+    let mut ac = oop::AveragedCollection::new();
+    ac.add(5);
+    println!("average is {}", ac.get_average());
+    ac.add(6);
+    println!("average is {}", ac.get_average());
+    ac.add(10);
+    println!("average is {}", ac.get_average());
+    ac.remove();
+    println!("average is {}", ac.get_average());
+    ac.remove();
+    println!("average is {}", ac.get_average());
+}
+
