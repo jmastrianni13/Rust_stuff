0773fb454973d6231351686065abf3cb7bb8f300
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-08 23:46:45 -0500

    polymorphism example added implementation

--- the_book/oop/src/main.rs
+++ the_book/oop/src/main.rs
@@ -1,6 +1,7 @@
 use oop;
 fn main() {
     demo_averagedcollection();
+    demo_draw();
 }
 
 fn demo_averagedcollection () {
@@ -15,5 +16,40 @@ fn demo_averagedcollection () {
     println!("average is {}", ac.get_average());
     ac.remove();
     println!("average is {}", ac.get_average());
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl oop::Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
+
+fn demo_draw () {
+    let screen = oop::Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(oop::Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
 }
 
