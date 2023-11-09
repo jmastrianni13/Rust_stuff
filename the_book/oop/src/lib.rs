66579d1bb3e4525fc30f17d1b170a1fd5cb1a2bd
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-08 23:30:06 -0500

    polymorphism example added trait and struct

--- the_book/oop/src/lib.rs
+++ the_book/oop/src/lib.rs
@@ -37,3 +37,18 @@ impl AveragedCollection {
     }
 }
 
+pub trait Draw {
+    fn draw(&self);
+}
+
+struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
