4778e8f98d58a6c3fa7d0b3beef873a9e736763e
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-10 21:12:22 -0500

    implemented parsing mathematical expressions

--- interpreters/lox/src/parser.rs
+++ interpreters/lox/src/parser.rs
@@ -21,20 +21,107 @@ impl Parser {
     fn equality(&mut self) -> expr::Expr {
         let mut exp = self.comparison();
 
-        while self.match_token(scanner::TokenType::BangEqual, scanner::TokenType::EqualEqual) {
+        while self.match_tokens(&[scanner::TokenType::BangEqual, scanner::TokenType::EqualEqual]) {
             let operator = self.previous();
             let rhs = self.comparison();
-            let exp = expr::Expr::Binary {
-                left: exp,
+            exp = expr::Expr::Binary {
+                left: Box::from(exp),
                 operator: operator,
-                right: rhs
+                right: Box::from(rhs),
             };
+        }
+        return exp;
+    }
 
-            return exp;
+    fn comparison(&mut self) -> expr::Expr {
+        let mut exp = self.term();
 
+        while self.match_tokens(
+            &[
+            scanner::TokenType::Greater,
+            scanner::TokenType::GreaterEqual,
+            scanner::TokenType::Less,
+            scanner::TokenType::LessEqual,
+            ]) {
+                let op = self.previous();
+                let rhs = self.term();
+                exp = expr::Expr::Binary {
+                    left: Box::from(exp),
+                    operator: op,
+                    right: Box::from(rhs),
+                }
         }
 
-        todo!();
+        return exp;
+    }
+
+    fn term(&mut self) -> expr::Expr {
+       let mut exp = self.factor();
+
+       while self.match_tokens(&[
+                               scanner::TokenType::Minus,
+                               scanner::TokenType::Plus
+       ]) {
+           let op = self.previous();
+           let rhs = self.factor();
+           exp = expr::Expr::Binary {
+               left: Box::from(exp),
+               operator: op,
+               right: Box::from(rhs),
+           };
+       }
+
+       return exp;
+    }
+
+    fn factor(&mut self) -> expr::Expr {
+        let mut exp = self.unary();
+        while self.match_tokens(&[
+                                scanner::TokenType::Slash,
+                                scanner::TokenType::Star
+        ]) {
+            let op = self.previous();
+            let rhs = self.unary();
+            exp = expr::Expr::Binary {
+                left: Box::from(exp),
+                operator: op,
+                right: Box::from(rhs),
+            };
+        }
+
+        return exp;
+    }
+
+    fn unary(&mut self) -> expr::Expr {
+        if self.match_tokens(&[
+                             scanner::TokenType::Bang,
+                             scanner::TokenType::Minus,
+        ]) {
+            let op = self.previous();
+            let rhs = self.unary();
+            return expr::Expr::Unary {
+                operator: op,
+                right: Box::from(rhs)
+            }
+        } else {
+            return self.primary();
+        }
+    }
+
+    fn primary(&mut self) -> expr::Expr {
+        if self.match_token(scanner::TokenType::LeftParen) {
+            let exp = self.expression();
+            self.consume(scanner::TokenType::RightParen, "expected ')'");
+            return expr::Expr::Grouping {
+                expression: Box::from(exp)
+            };
+        } else {
+            let token = self.peek();
+            self.advance();
+            return expr::Expr::Literal {
+                value: expr::LiteralValue::from_token(token)
+            };
+        }
     }
 
     fn consume(&mut self, token_type: scanner::TokenType, msg: &str) {
