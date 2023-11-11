97f5f36d468c232f16ed64c082e26e2cee9ffc53
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-10 21:11:37 -0500

    added match token logic

--- interpreters/lox/src/parser.rs
+++ interpreters/lox/src/parser.rs
@@ -37,5 +37,57 @@ impl Parser {
         todo!();
     }
 
+    fn consume(&mut self, token_type: scanner::TokenType, msg: &str) {
+        let token = self.peek();
+        if token.token_type == token_type {
+            self.advance();
+        } else {
+            panic!("{}", msg);
+        }
+    }
+
+    fn match_token(&mut self, typ: scanner::TokenType) -> bool {
+        if self.is_at_end() {
+            return false;
+        } else {
+            if self.peek().token_type == typ {
+                self.advance();
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    fn match_tokens(&mut self, typs: &[scanner::TokenType]) -> bool {
+        for typ in typs {
+            if self.match_token(*typ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    fn advance(&mut self) -> scanner::Token {
+        if !self.is_at_end() {
+            self.current += 1;
+        }
+
+        return self.previous();
+
+    }
+
+    fn peek(&mut self) -> scanner::Token {
+        return self.tokens[self.current].clone();
+    }
+
+    fn previous(&mut self) -> scanner::Token {
+        return self.tokens[self.current - 1].clone();
+    }
+
+    fn is_at_end(&mut self) -> bool {
+        return self.peek().token_type == scanner::TokenType::Eof;
+    }
 }
 
