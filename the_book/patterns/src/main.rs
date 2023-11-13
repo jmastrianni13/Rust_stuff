3a8e77d9b41a7359380613dea2069772863e55bf
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-12 21:48:20 -0500

    initial commit, added if let expression

---
+++ the_book/patterns/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    demo_if_let_exp();
+}
+
+fn demo_if_let_exp () {
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using you favorite color, {color}, as the background.");
+    } else if is_tuesday {
+        println!("Tuesday is greenday!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+}
+
+
+
+
