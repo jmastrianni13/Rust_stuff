3840e0575e72b21ab2758d2b637b3718ddccb299
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-14 22:15:54 -0500

    added ignoring to match examples

--- the_book/patterns/src/main.rs
+++ the_book/patterns/src/main.rs
@@ -51,6 +51,12 @@ struct Point {
     y: i32,
 }
 
+struct Point3d {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
@@ -116,6 +122,50 @@ fn demo_matching() {
         'k'..='z' => println!("late ACSII letter"),
         _ => println!("something else"),
     }
+
+    // ignoring values with _
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {first}, {third}, {fifth}");
+        }
+    }
+
+    let s = Some(String::from("Hello!"));
+
+    if let Some(_) = s { // _ does not bind, so s is not moved
+        println!("found a string");
+    }
+
+    println!("{:?}", s);
+
+    // ignoring parts of a value with ..
+    let origin = Point3d { x: 0, y: 0, z: 0};
+
+    match origin {
+        Point3d { x, ..} => println!("x is {}", x),
+    }
+
+    match numbers { // defined above
+        (first, .., last) => {
+            println!("Some numbers: {first}, {last}");
+        }
+    }
 }
 
 fn demo_destructuring() {
@@ -165,5 +215,8 @@ fn demo_destructuring() {
         }
         _ => (),
     }
+
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+    println!("feet = {feet}, inches = {inches}, x = {x}, y = {y}");
 }
 
