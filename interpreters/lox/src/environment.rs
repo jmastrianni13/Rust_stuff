4f90e519169b99bd35522d64da7b9e486e326eef
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-16 03:45:44 -0500

    added methods

--- interpreters/lox/src/environment.rs
+++ interpreters/lox/src/environment.rs
@@ -5,3 +5,19 @@ pub struct Environment {
     values: HashMap<String, expr::LiteralValue>,
 }
 
+impl Environment {
+    pub fn new() -> Self {
+        return Self {
+            values: HashMap::new()
+        };
+    }
+
+    pub fn define(&mut self, name: String, value: expr::LiteralValue) {
+        self.values.insert(name, value);
+    }
+
+    pub fn get(&self, name: String) -> Option<&expr::LiteralValue> {
+        return self.values.get(&name);
+    }
+}
+
