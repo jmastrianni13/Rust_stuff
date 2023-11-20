a7695fcdcedc02f0f190b86095e43793902a9024
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-19 23:39:15 -0500

    remove debugging print for string, done with scoping for now

--- interpreters/lox/src/interpreter.rs
+++ interpreters/lox/src/interpreter.rs
@@ -27,7 +27,7 @@ impl Interpreter {
                         Rc::get_mut(&mut self.environment)
                         .expect("could not get mutable reference to environment"),
                         )?;
-                    println!("{:?}", value);
+                    println!("\"{}\"", value.to_string());
                 }
                 stmt::Stmt::Var { name, initializer } => {
                     let value = initializer.evaluate(
