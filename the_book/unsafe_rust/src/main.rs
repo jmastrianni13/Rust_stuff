0de2f310291d552b41ee9e53fd4b8ac895d14e7f
Joe Mastrianni <jmastrianni13@gmail.com>
2023-11-20 01:11:55 -0500

    example of dereferencing raw pointers

--- the_book/unsafe_rust/src/main.rs
+++ the_book/unsafe_rust/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    demo_raw_pointers();
+}
+
+fn demo_raw_pointers() {
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    println!("num is {num} at {:p}", &num); // :p prevent println! automatically dereferencing
+    println!("r1 is {r1:?} at {:p}", &r1);
+    println!("r2 is {r2:?} at {:p}", &r2);
+
+    // no good reason to write code like the following
+    let address = 0x012345usize; // arbitrary address
+    let r = address as *const i32;
+    println!("r is {r:?} at {:p}", &r);
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
 }
