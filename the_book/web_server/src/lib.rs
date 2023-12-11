e1d89a845823c3a487458cea3f51d38896d556ab
Joe Mastrianni <jmastrianni13@gmail.com>
2023-12-10 21:42:44 -0500

    wip replaced threads with workers in ThreadPool

--- the_book/web_server/src/lib.rs
+++ the_book/web_server/src/lib.rs
@@ -1,8 +1,7 @@
 use std::thread;
 
-#[derive(Debug)]
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
@@ -16,18 +15,31 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
-            todo!();
+        for id in 0..size {
+            workers.push(Worker::new(id));
         }
 
-        return ThreadPool { threads };
+        return ThreadPool { workers };
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+    }
+}
+
+struct Worker {
+    id: usize,
+    w_thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let w_thread = thread::spawn(|| {});
+
+        return Worker { id, w_thread };
     }
 }
