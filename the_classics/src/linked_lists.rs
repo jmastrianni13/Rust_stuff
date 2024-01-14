2701f5a5ef478acaf51a1043a91548540f5ddb30
Joe Mastrianni <jmastrianni13@gmail.com>
2024-01-14 15:15:43 -0500

    wip singly linked list add print fn

--- the_classics/src/linked_lists.rs
+++ the_classics/src/linked_lists.rs
@@ -9,10 +9,19 @@ pub fn main() {
     println!("{:?}", my_node);
 
     let mut my_sll: SingleLinkedList<i32> = SingleLinkedList::new();
-    println!("{:?}", my_sll);
     my_sll.insert(5);
     my_sll.insert(10);
-    println!("{:?}", my_sll);
+    my_sll.insert(20);
+    my_sll.insert(40);
+    print_sll(&my_sll);
+
+    let mut my_sll: SingleLinkedList<char> = SingleLinkedList::new();
+    my_sll.insert('e');
+    my_sll.insert('d');
+    my_sll.insert('c');
+    my_sll.insert('b');
+    my_sll.insert('a');
+    print_sll(&my_sll);
 }
 
 struct Node<T> {
@@ -51,3 +60,11 @@ impl<T> SingleLinkedList<T> {
     }
 }
 
+fn print_sll<T: fmt::Debug>(sll: &SingleLinkedList<T>) {
+    let mut curr_node = &sll.head;
+    while let Some(node) = curr_node {
+        print!("{:?}->", node.data.as_ref().unwrap());
+        curr_node = &node.next;
+    }
+    println!("{:?}", curr_node);
+}
