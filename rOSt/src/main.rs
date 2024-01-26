431462cb7ea215dd4a7311f000e7aad3e530f552
Joe Mastrianni <jmastrianni13@gmail.com>
2024-01-26 00:02:35 -0500

    remove C entrypoint

--- rOSt/src/main.rs
+++ rOSt/src/main.rs
@@ -1,10 +1,15 @@
 #![no_std]
-use core::panic::PanicInfo;
+#![no_main]
 
-fn main() {}
+use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
     loop {}
 }
 
