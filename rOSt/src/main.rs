cf7043a5cbe12ae06426aa34475c6183ea50cba5
Joe Mastrianni <jmastrianni13@gmail.com>
2024-02-03 23:59:07 -0500

    added vga buffer

--- rOSt/src/main.rs
+++ rOSt/src/main.rs
@@ -2,6 +2,7 @@
 #![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 // This function is called on panic
 #[panic_handler]
@@ -13,15 +14,7 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
-
