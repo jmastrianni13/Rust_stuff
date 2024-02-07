e0ccebcb182852915a9d6324a46b6f8280f3fdab
Joe Mastrianni <jmastrianni13@gmail.com>
2024-02-06 22:34:39 -0500

    added println to panic handler

--- rOSt/src/main.rs
+++ rOSt/src/main.rs
@@ -6,13 +6,15 @@ mod vga_buffer;
 
 // This function is called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
