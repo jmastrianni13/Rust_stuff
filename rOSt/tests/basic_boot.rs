1c8bb4cc897678c25740e9a1cc1e24f004d53153
Joe Mastrianni <jmastrianni13@gmail.com>
2024-02-08 23:56:04 -0500

    added tests

--- rOSt/tests/basic_boot.rs
+++ rOSt/tests/basic_boot.rs
@@ -5,6 +5,7 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use rOSt::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -18,3 +19,7 @@ fn panic(info: &PanicInfo) -> ! {
     rOSt::test_panic_handler(info)
 }
 
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
