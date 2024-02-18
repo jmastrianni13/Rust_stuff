deb365c8e2684e7b2248bd6dc11c66afd17eea45
Joe Mastrianni <jmastrianni13@gmail.com>
2024-02-18 14:25:28 -0500

    added exception handling

--- rOSt/src/main.rs
+++ rOSt/src/main.rs
@@ -11,9 +11,15 @@ use rOSt::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    rOSt::init();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
