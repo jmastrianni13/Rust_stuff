89d6e103145f2a751850cc0ca886b1be83912579
Joe Mastrianni <jmastrianni13@gmail.com>
2024-03-01 01:23:06 -0500

    added oob read and write

--- rOSt/src/main.rs
+++ rOSt/src/main.rs
@@ -13,10 +13,17 @@ pub extern "C" fn _start() -> ! {
 
     rOSt::init();
 
-    let ptr = 0xdeadbeaf as *mut u8;
+    let ptr = 0x2031b2 as *mut u8;
+
+    unsafe {
+        let x = *ptr;
+    }
+    println!("read worked");
+
     unsafe {
         *ptr = 42;
     };
+    println!("write worked");
 
     #[cfg(test)]
     test_main();
